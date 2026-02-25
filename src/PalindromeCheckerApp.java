6f5a7d66e573f3d206944d8826bddff71eec61cc
Yogapriyan14 <yl9442@srmist.edu.in>
2026-02-25 11:14:04 +0530

    UC1

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,5 +1,11 @@
 public class PalindromeCheckerApp {
+    public static void main(String[] args) {
 
+
+        System.out.println("Welcome to the Palindrome Checker Application!");
+        System.out.println("This application validates whether a given string is a palindrome.");
+
+        System.out.println("Program initialized successfully.");
 
     }
 }
